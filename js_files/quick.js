653d5f4d4e5315d20ad9678ebde0078100356ad9
Sudhanshu Kumar Sinha <sudhanshukr2003@gmail.com>
2023-09-01 12:21:23 +0530

    first commit

---
+++ js_files/quick.js
@@ -0,0 +1,139 @@
+const button4 = document.getElementById("Button4");
+
+button4.addEventListener("click", quickSortCall.bind(null));
+
+function quickSortCall(){
+    num = Number(document.getElementById("arr_sz").value);
+    quickSort(0,num-1);
+}
+
+
+async function partition(start, end){
+    let bars = document.querySelectorAll(".bar");//. 
+    let i= start + 1;
+    let piv = Number(bars[start].childNodes[0].innerHTML) ;            //make the first element as pivot element.
+    bars[start].style.backgroundColor="#6b5b95";
+    for(let j =start + 1; j <= end ; j++ )  {
+    /*rearrange the array by putting elements which are less than pivot
+       on one side and which are greater that on other. */
+
+       
+
+       // To pause the execution of code for 600 milliseconds
+                    await new Promise((resolve) =>
+                    setTimeout(() => {
+                    resolve();
+                    }, 100)
+                );
+
+                console.log( "j="+j+ "bars[j]= "+Number(bars[j].childNodes[0].innerHTML)+ " pivot= "+ piv)
+          if ( Number(bars[j].childNodes[0].innerHTML) < piv) {
+
+                if(j!=i){
+
+                    
+
+              
+                 //swap (bars[ i ],bars[ j ]);
+                                    
+                // Changing the color of elements to be swapped 
+                bars[i].style.backgroundColor = "#ffd32a"; //#6b5b95 
+                bars[j].style.backgroundColor = "#ff4242"; //rgb(0, 183, 255)
+
+                // To pause the execution of code for 600 milliseconds
+                await new Promise((resolve) =>
+                setTimeout(() => {
+                resolve();
+                }, 200)
+                );
+
+                temp1 = bars[j].style.height; 
+                temp2 = bars[j].childNodes[0].innerText; 
+                bars[j].style.height = bars[i].style.height; 
+                bars[i].style.height = temp1; 
+                bars[j].childNodes[0].innerText = bars[i].childNodes[0].innerText; 
+                bars[i].childNodes[0].innerText = temp2;
+
+                // To pause the execution of code for 600 milliseconds
+                await new Promise((resolve) =>
+                setTimeout(() => {
+                resolve();
+                }, 200)
+                );
+
+                            
+                //  // Changing the color to the previous one 
+                bars[i].style.backgroundColor = "rgb(0, 183, 255)"; //#6b5b95 
+                bars[j].style.backgroundColor = "rgb(0, 183, 255)"; //rgb(0, 183, 255)  
+                }
+
+                bars[j].style.backgroundColor = "rgb(0, 183, 255)"; //#6b5b95 
+
+                        i += 1;
+                    }
+            }
+                    // To pause the execution of code for 600 milliseconds
+                    await new Promise((resolve) =>
+                    setTimeout(() => {
+                    resolve();
+                    }, 200)
+                );
+
+        //put the pivot element in its proper place.
+            temp1 = bars[start].style.height; 
+            temp2 = bars[start].childNodes[0].innerText; 
+            bars[start].style.height = bars[i-1].style.height; 
+            bars[i-1].style.height = temp1; 
+            bars[start].childNodes[0].innerText = bars[i-1].childNodes[0].innerText; 
+            bars[i-1].childNodes[0].innerText = temp2;
+                            
+
+                        // To pause the execution of code for 600 milliseconds
+                        await new Promise((resolve) =>
+                        setTimeout(() => {
+                        resolve();
+                        }, 100)
+                    );
+    
+                    // for(k=start +1;k<end;k++){
+                    //     bars[k].style.backgroundColor="rgb(49, 226, 13)";
+                    // }
+   return i-1;                      //return the position of the pivot
+}
+
+
+async function quickSort(low, high) {
+let count =0;
+let bars = document.querySelectorAll(".bar");//. 
+
+   if(low<high){
+       
+       let j= await partition(low,high);
+       if(low < j-1)
+              // To pause the execution of code for 600 milliseconds
+              await new Promise((resolve) =>
+              setTimeout(() => {
+              resolve();
+              }, 100)
+          );
+
+       await quickSort(low,j-1);
+       for(i=low;i<=j;i++){
+            bars[i].style.backgroundColor="#13CE66";
+       }
+       //console.log(++count);
+              // To pause the execution of code for 600 milliseconds
+              await new Promise((resolve) =>
+              setTimeout(() => {
+              resolve();
+              }, 100)
+          );
+       await quickSort(j+1,high);
+       for(i=j+1;i<=high;i++){
+        bars[i].style.backgroundColor="#13CE66";
+   }
+       //console.log(++count);
+   }
+
+   enable();
+}
